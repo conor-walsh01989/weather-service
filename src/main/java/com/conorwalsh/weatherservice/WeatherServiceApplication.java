61053506dd2886d42d381e22283ea4d7e88209b9
conor-walsh01989 <conor.walsh01989@gmail.com>
2021-02-18 22:06:28 +0000

    Moved conversion tests

--- src/main/java/com/conorwalsh/weatherservice/WeatherServiceApplication.java
+++ src/main/java/com/conorwalsh/weatherservice/WeatherServiceApplication.java
@@ -5,18 +5,12 @@ import java.util.List;
 
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.spi.MappingContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.conorwalsh.weatherservice.client.response.CurrentWeatherResponse;
-import com.conorwalsh.weatherservice.client.response.Weather;
-import com.conorwalsh.weatherservice.model.dto.WeatherDescriptionDto;
-import com.conorwalsh.weatherservice.model.dto.WeatherDetailDto;
-import com.conorwalsh.weatherservice.model.dto.WeatherDto;
-import com.conorwalsh.weatherservice.model.dto.WindDto;
 import com.conorwalsh.weatherservice.utilities.DtoMapper;
+
 
 @SpringBootApplication
 public class WeatherServiceApplication {
@@ -30,7 +24,7 @@ public class WeatherServiceApplication {
 	public ModelMapper modelMapper() {
 		ModelMapper mapper = new ModelMapper();
 		//Custom converter to handle property mappings	
-		mapper.addConverter(DtoMapper.getWeatherConverter());
+		mapper.addConverter(new DtoMapper());
 		return mapper;
 	}
 
---
+++ src/test/java/com/conorwalsh/weatherservice/utilities/DtoMapperTest.java
@@ -0,0 +1,59 @@
+package com.conorwalsh.weatherservice.utilities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+
+import com.conorwalsh.weatherservice.client.response.Coordinates;
+import com.conorwalsh.weatherservice.client.response.CurrentWeatherResponse;
+import com.conorwalsh.weatherservice.client.response.Weather;
+import com.conorwalsh.weatherservice.model.dto.WeatherDto;
+
+class DtoMapperTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testWeatherResponseToDtoConversion() {
+		//Given - Source object has been populated
+		CurrentWeatherResponse sampleResponse = new CurrentWeatherResponse();
+		sampleResponse.setName("London");
+		Coordinates coord = new Coordinates();
+		coord.setLat(44);
+		coord.setLon(22.55);
+		sampleResponse.setCoord(coord);
+		List<Weather> detailList = new ArrayList<Weather>();
+		Weather weatherDetail = new Weather();
+		weatherDetail.setId(1);
+		weatherDetail.setDescription("Cloudy today");
+		weatherDetail.setMain("Cloudy");
+		detailList.add(weatherDetail);
+		sampleResponse.setWeather(detailList);
+	
+		//When - Source object is mapped to target
+		ModelMapper mapper = new ModelMapper();
+		mapper.addConverter(new DtoMapper());
+		WeatherDto weather = mapper.map(sampleResponse, WeatherDto.class);
+	
+		//Then - Properties are as expected
+		assert weather.getCityName().equals("London");	
+		assert weather.getLatitude() == 44;
+		assert weather.getLongitude() == 22.55;
+		assert weather.getWeatherDescription().size() == 1;
+		assert weather.getWeatherDescription().get(0).getDescription().equals("Cloudy today");
+		assert weather.getWeatherDescription().get(0).getMain().equals("Cloudy");
+	}
+	
+}
